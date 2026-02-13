526346c0574a3a26dc640a7bb0c73b2e3c2d5154
Hammad Dalvi <hammaddalvi08@gmail.com>
2026-02-14 00:18:31 +0530

    Added custom API Handling

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@ import mongoose from "mongoose";
 import { DB_Name } from "./constant.js";
 import connectDb from "./db/Index.js";
 import dotenv from "dotenv"
+import app from "./app.js";
+
 
 dotenv.config({
     path: './env'
 })
 
-connectDb();
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        app.on ("error",(error) =>{
+            console.log("ERROR: ", error);
+            throw error
+            
+        } )
+        
+    })
+})
+.catch((err) =>{
+    console.log("MongoDB connection failed !!!",err);
+    
+})
